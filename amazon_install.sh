5bffe4720c8cb4d6b4e7e31960fab495129d71b9
Yevgeni Reif <yevgeni.reif@intel.com>
2016-05-31 10:35:41 +0300

    Update amazon_install.sh

--- amazon_install.sh
+++ amazon_install.sh
@@ -2,29 +2,29 @@
 # Get cluster parameters
 echo "Please enter the cluster size: "
 read cluster_size
-if (("$cluster_size" > "16")); then
-    echo "Your cluster size must be less than 16"
+if (("$cluster_size" > "32")); then
+    echo "Your cluster size must be less than 32"
     exit 0
 fi
 
-mkdir -p /tmp/dl-genie/
-echo "[dlgenie]" > /tmp/dl-genie/amazon_hosts
+mkdir -p $HOME/dl-genie/
+echo "[dlgenie]" > $HOME/dl-genie/amazon_hosts
 for ((i = 1; i <= cluster_size; i++))
 do
     echo "Please enter server ${i} IP: "
     read server_ip
     all_ips=$all_ips" $server_ip";#concatencate all ips
-    echo "$server_ip" >> /tmp/dl-genie/amazon_hosts
+    echo "$server_ip" >> $HOME/dl-genie/amazon_hosts
 done
 
-echo "[dlgenie:vars]" >> /tmp/dl-genie/amazon_hosts
-echo "ansible_ssh_user=core" >> /tmp/dl-genie/amazon_hosts
-echo 'ansible_python_interpreter="PATH=/home/core/bin:$PATH python"' >> /tmp/dl-genie/amazon_hosts
+echo "[dlgenie:vars]" >> $HOME/dl-genie/amazon_hosts
+echo "ansible_ssh_user=core" >> $HOME/dl-genie/amazon_hosts
+echo 'ansible_python_interpreter="PATH=/home/core/bin:$PATH python"' >> $HOME/dl-genie/amazon_hosts
 
 #check if private-key exists
-while [ ! -f /tmp/dl-genie/private-key.pem ]
+while [ ! -f $HOME/dl-genie/private-key.pem ]
 do
-    echo "Private key could not be found. Please save your private key pem file as: /tmp/dl-genie/private-key.pem"
+    echo "Private key could not be found. Please save your private key pem file as: $HOME/dl-genie/private-key.pem"
     echo "Press any key when done..."
     read dummy
 done
@@ -33,6 +33,8 @@ DL_GENIE_ANSIBLE_CONTAINER=intelaa/dl-genie-ansible:0.0.1
 DL_GENIE_ANSIBLE_CONTAINER_NAME=dl-genie-ansible
 
 docker pull ${DL_GENIE_ANSIBLE_CONTAINER}
-docker run --rm -it --volume=/tmp/dl-genie:/workspace --name ${DL_GENIE_ANSIBLE_CONTAINER_NAME} ${DL_GENIE_ANSIBLE_CONTAINER} ansible-playbook /etc/ansible/deploy_amazon_dlgenie.yml -i /workspace/amazon_hosts --private-key=/workspace/private-key.pem -vvvv
+docker run --rm -it --volume=$HOME/dl-genie:/workspace --name ${DL_GENIE_ANSIBLE_CONTAINER_NAME} ${DL_GENIE_ANSIBLE_CONTAINER} ansible-playbook /etc/ansible/deploy_amazon_dlgenie.yml -i /workspace/amazon_hosts --private-key=/workspace/private-key.pem -vvvv
 
+docker exec -i dlgenie sh -c 'cat > ~/.ssh/private-key.pem' < $HOME/dl-genie/private-key.pem
+docker exec -i dlgenie sh -c 'cat > ~/.ssh/config' < "IdentityFile ~/.ssh/private-key.pem"
 docker exec -it dlgenie /bin/bash /opt/configureSSHD.sh $all_ips
